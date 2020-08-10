89454416b67e09466480d8f1727ce4cbb1bb49b0
albervan.davies <albervan.davies@siagri.com.br>
2020-08-10 01:18:10 -0400

    Aula 5 - CriaÃ§Ã£o das tabelas do bd
    Co-Authored-By: Albervan Bergamasco Davies <54541416+albervan@users.noreply.github.com>

---
+++ src/database/db.js
@@ -0,0 +1,34 @@
+const database = require('sqlite-async')
+
+function execute(db) {
+    //Criar as tabelas do BD
+    db.exec(`
+        CREATE TABLE IF NOT EXISTS proffys (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            avatar TEXT,
+            whatsapp TEXT,
+            bio TEXT
+        );
+
+        CREATE TABLE IF NOT EXISTS classes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            subject TEXT,
+            cost TEXT,
+            proffy_id INTEGER
+        );
+
+        CREATE TABLE IF NOT EXISTS class_schedule (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            class_id INTEGER,
+            weekday INTEGER,
+            time_from INTEGER,
+            time_to INTEGER
+        );
+    `)
+}
+
+module.exports = database.open(__dirname + '/database.sqlite').then(execute)
+// Se não existir o arquivo do bd, ele é criado nesse momento
+// o "then" garante que o banco de dados estará aberto ao continuar a execução
+// "module.exports" me permite disponibilizar o objeto para ser chamado em outro local pelo "require"
