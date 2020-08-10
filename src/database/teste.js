139378b7fea837493882bedad665c8b03bdc9090
albervan.davies <albervan.davies@siagri.com.br>
2020-08-10 18:33:47 -0400

    Aula 5 - ExercÃ­cio para inclusÃ£o no BD
    Essa implementaÃ§Ã£o esta apresentando erro.
    
    Co-Authored-By: Albervan Bergamasco Davies <54541416+albervan@users.noreply.github.com>

---
+++ src/database/teste.js
@@ -0,0 +1,41 @@
+const database = require('./db')
+const createProffy = require('./createProffy')
+
+database.then(async (db) => {
+    // Inserir dados
+
+    proffyValue = {
+        name: 'Mayk Brito',
+        avatar: 'https://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4',
+        whatsapp: '9876545678',
+        bio: 'An instructor focused on helping people start programming for web'
+        //  - #html #css #javascript #sql #react #nodejs #fullstack'
+    }
+
+    classValue = {
+        subject: 'Matemática',
+        cost: '20'
+        //proffy_id
+    }
+
+    classScheduleValues = [
+        {
+            weekday: 1,
+            time_from: 720,
+            time_to: 1220
+            //class_id
+        },
+        {
+            weekday: 0,
+            time_from: 520,
+            time_to: 1220
+            //class_id
+        }
+    ]
+
+    await createProffy(db, { proffyValue, classValue, classScheduleValues })
+
+    //Consultar dados
+    // const selectedProffys = await db.all("SELECT * FROM proffys")
+    // console.log(selectedProffys)
+})
