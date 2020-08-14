ca4e484c9a295b52da83301e5125d360878c0bb9
albervan.davies <albervan.davies@siagri.com.br>
2020-08-14 02:20:33 -0400

    Aula 5 - ExercÃ­cios de consultar dados
    Ajustes no db.js para receber o nÃºmero da matÃ©ria;
    
    Co-Authored-By: Albervan Bergamasco Davies <54541416+albervan@users.noreply.github.com>

--- src/database/teste.js
+++ src/database/teste.js
@@ -2,8 +2,7 @@ const Database = require('./db')
 const createProffy = require('./createProffy')
 
 Database.then(async (db) => {
-    // Inserir dados
-
+// Inserir dados
     proffyValue = {
         name: "Mayk Brito",
         avatar: "https://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
@@ -13,7 +12,7 @@ Database.then(async (db) => {
     }
 
     classValue = {
-        subject: "Matemática",
+        subject: 4,
         cost: "20"
         //proffy_id
     }
@@ -35,7 +34,24 @@ Database.then(async (db) => {
 
     await createProffy(db, { proffyValue, classValue, classScheduleValues })
 
-    //Consultar dados
-    // const selectedProffys = await db.all("SELECT * FROM proffys")
-    // console.log(selectedProffys)
+//Consultar dados
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+    console.log(selectedProffys)
+
+    const selectedClassProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys JOIN classes ON (proffys.id = classes.proffy_id)
+        WHERE classes.proffy_id = 3;
+    `)
+    console.log(selectedClassProffys)
+
+    const selectedScheduleClass = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = 1
+        AND class_schedule.weekday = 0
+        AND class_schedule.time_from <= 620
+        AND class_schedule.time_to > 620
+    `)
+    console.log(selectedScheduleClass)
 })
